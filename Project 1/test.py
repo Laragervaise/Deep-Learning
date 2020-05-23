ac891258cd95598052806f0339ef7a34fd7b9445
Kenyu Kobayashi <kenyukobayashi@hotmail.com>
2020-05-23 04:01:05 +0200

    FinalTests

--- Project 1/test.py
+++ Project 1/test.py
@@ -3,15 +3,16 @@ import argparse
 import sys
 import torch
 
-# Model selection
+# Uncomment this part if you would like to choose the model to train
 
+torch.manual_seed(0)
+
+"""
 parser = argparse.ArgumentParser()
 
 parser.add_argument("model", help="one of the following: FNN, FNN_WS, FNN_AUX, FNN_WS_AUX, CNN, CNN_WS, CNN_AUX, CNN_WS_AUX")
 
 args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
-
-torch.manual_seed(8)
 
 train_input1, train_input2, test_input1, test_input2, train_classes1, train_classes2, test_classes1, test_classes2, train_target, test_target = load()
 model_FNN, model_FNN_WS, model_FNN_AUX, model_FNN_WS_AUX, model_CNN, model_CNN_WS, model_CNN_AUX, model_CNN_WS_AUX = initialize_models()
@@ -40,13 +41,46 @@ elif args.model.upper() == 'CNN_AUX':
 elif args.model.upper() == 'CNN_WS_AUX':
 	history = train(model_CNN_WS_AUX, train_input1, train_input2, train_classes1,  train_classes2,train_target, \
                         test_input1, test_input2, test_classes1, test_classes2,  test_target)
-
-
-if args.plot:
-    plotacc([history],  args.model.upper())
-    plotLoss([history], args.model.upper())
-
+    
 print(args.model.upper())
 
+"""
+
+# Running all of the models
+
+train_input1, train_input2, test_input1, test_input2, train_classes1, train_classes2, test_classes1, test_classes2, train_target, test_target = load()
+model_FNN, model_FNN_WS, model_FNN_AUX, model_FNN_WS_AUX, model_CNN, model_CNN_WS, model_CNN_AUX, model_CNN_WS_AUX = initialize_models()
+
+print("Model_FNN : \n")
+history = train(model_FNN, train_input1, train_input2, train_classes1,  train_classes2,train_target, \
+                        test_input1, test_input2, test_classes1, test_classes2,  test_target)
+
+print("\n\n Model_FNN_WS : \n")
+history = train(model_FNN_WS, train_input1, train_input2, train_classes1,  train_classes2,train_target, \
+                        test_input1, test_input2, test_classes1, test_classes2,  test_target)
+
+print("\n\n Model_FNN_AUX : \n")
+history = train(model_FNN_AUX, train_input1, train_input2, train_classes1,  train_classes2,train_target, \
+                        test_input1, test_input2, test_classes1, test_classes2,  test_target)
+
+print("\n\n Model_FNN_WS_AUX : \n")
+history = train(model_FNN_WS_AUX, train_input1, train_input2, train_classes1,  train_classes2,train_target, \
+                        test_input1, test_input2, test_classes1, test_classes2,  test_target)
+
+print("\n\n Model_CNN : \n")
+history = train(model_CNN, train_input1, train_input2, train_classes1,  train_classes2,train_target, \
+                        test_input1, test_input2, test_classes1, test_classes2,  test_target)
+
+print("\n\n Model_CNN_WS : \n")
+history = train(model_CNN_WS, train_input1, train_input2, train_classes1,  train_classes2,train_target, \
+                        test_input1, test_input2, test_classes1, test_classes2,  test_target)
+
+print("\n\n Model_FNN_AUX : \n")
+history = train(model_CNN_AUX, train_input1, train_input2, train_classes1,  train_classes2,train_target, \
+                        test_input1, test_input2, test_classes1, test_classes2,  test_target)
+
+print("\n\n Model_FNN_WS_AUX : \n")
+history = train(model_CNN_WS_AUX, train_input1, train_input2, train_classes1,  train_classes2,train_target, \
+                        test_input1, test_input2, test_classes1, test_classes2,  test_target)
 
 
--- Project 2/test.py
+++ Project 2/test.py
@@ -29,17 +29,20 @@ criterions = ['MSE', 'CROSSENTROPY', 'CROSSENTROPY', 'CROSSENTROPY', 'CROSSENTRO
 learning_decay_types = ['exponential', 'exponential', 'time-based', 'exponential', \
                         'exponential', 'exponential', 'time-based', 'exponential', 'exponential']
 
+# Load data
+train_pts, test_pts, train_labels, test_labels, train_targets, test_targets = load_data()
 
+
+# Uncomment this part if you would like to select the model to run 
+
+"""
 # Model selection
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("model", help="one of the following: RELU, RELU_SIGMOID, RELU_TANH, RELU_SOFTMAX, LEAKY_RELU, LEAKY_RELU_SIGMOID, LEAKY_RELU_TANH, LEAKY_RELU_SOFTMAX, TANH")
 
-
 args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
-
-train_pts, test_pts, train_labels, test_labels, train_targets, test_targets = load_data()
 
 
 if args.model.upper() == 'RELU':
@@ -88,6 +91,11 @@ elif args.model.upper() == 'TANH':
           criterion = criterions[8], batch_size = 1000, optimizer = 'MiniBatch')
 
 print("\n",args.model.upper())
+"""
 
+# Running a single model 
 
-
+print("Model with ReLU and Tanh activation functions:")
+history = train(model_selector(2, seed = 0), train_pts, train_targets, epochs[2], lrs[2], test_pts = test_pts, test_targets = test_targets, 
+          train_history = True, test_history = True, weight_decay = weight_decays[2], lr_decay_type = learning_decay_types[2], lr_decay = learning_decays[2], 
+          criterion = criterions[2], batch_size = 1000, optimizer = 'MiniBatch')
